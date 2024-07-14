79db12396db0549f3045472595481bde543a88dd
Younis Yousaf <yykhan789@outlook.com>
2024-07-15 00:08:04 +0500

    Day 1 Activities

---
+++ Day1/Variable_Delcaration.js
@@ -0,0 +1,30 @@
+// Task 1: Declare Variable using var keyword
+var myNum = 2;
+console.log(myNum);
+
+// Task 2: Declare Variable using let keyword
+let myName = "John";
+console.log(myName);
+
+// Activity 2: Constant Declaration
+// Task 3: Declare Variable using const keyword
+const isAdmin = true;
+console.log(isAdmin);
+
+// Task 4: Different types of variable declaration
+let myVar = 2; // number
+console.log(myVar);
+let myNewName = "John"; // string
+console.log(myNewName);
+let myBoolean = true; // boolean
+console.log(myBoolean);
+let myNull = null; // object
+console.log(myNull);
+let myUndefined = undefined; // undefined
+console.log(myUndefined);
+let mySymbol = Symbol("id"); // symbol
+console.log(mySymbol);
+let myArr = [1, 2, 3]; // array
+console.log(myArr);
+let myObj = { name: "John", age: 30 }; // object
+console.table(myObj);
